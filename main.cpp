e4609c54ad66b94fd10b436848915e3d62c39b9b
poosana <59276782+poosana@users.noreply.github.com>
2019-12-27 17:04:14 +0700

    Add files via upload
    lab3_3 ข้อ3

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  lab3
+//
+//  Created by Poosana Thassanavisut on 27/12/2562 BE.
+//  Copyright © 2562 Poosana Thassanavisut. All rights reserved.
+//
+
+#include<iostream>
+
+using namespace std;
+int main ()
+{
+    float Denomenator = 2;
+    float sum = 0;
+    while(Denomenator <= 69){sum = sum +(1/Denomenator);
+        Denomenator++;
+    }
+    cout << sum;
+    return 0;
+    }
+        
+    
+    
+
+   
+
